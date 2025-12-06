707abfc5b9b9ace5d18cc6cc264e2f38642c7d23
vorw <sultanalsanie23@gmail.com>
2025-12-06 16:59:23 +0000

    Task

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,13 +43,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		dlistint_t *node = *head;
+		tmp = *head;
 
-		if (node->prev)
-			node->prev->next = node->next;
-		if (node->next)
-			node->next->prev = node->prev;
-		free(*head);
+		(*head)->prev->next = (*head)->next;
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		free(tmp);
 		*head = saved_head;
 	}
 	return (1);
